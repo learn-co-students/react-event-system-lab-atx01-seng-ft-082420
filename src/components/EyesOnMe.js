e42f864d6c6849e83ae9dbea0a3043e9f23d11a6
jlonetree <jlonetree@gmail.com>
2020-10-28 00:58:45 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
-// Code EyesOnMe Component Here
+import React, {Component} from 'react'
+
+class EyesOnMe extends Component {
+
+    handleFocusOutput = () => console.log('Good!')
+    handleBlurOutput = () => console.log('Hey! Eyes on me!')
+
+    render() {
+        return (
+            <button onFocus={this.handleFocusOutput} onBlur={this.handleBlurOutput}/>
+        )
+    }
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
+import React, {Component} from "react"
 // Code Keypad Component Here
+
+class Keypad extends Component {
+
+    handleInputPassword = () => console.log('Entering password...')
+
+    render() {
+        return(
+        <input type="password" onKeyUp={this.handleInputPassword}/>
+        )
+    }
+}
+
+export default Keypad
